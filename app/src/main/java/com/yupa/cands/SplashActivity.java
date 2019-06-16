ee6172ddefb31e2e98c85f5b1a2897851f35acd3
pyunqi <pyunqi@gmail.com>
2019-06-16 23:22:31 +1200

    init wiht splash screen

---
+++ app/src/main/java/com/yupa/cands/SplashActivity.java
@@ -0,0 +1,27 @@
+package com.yupa.cands;
+
+import android.content.Intent;
+import android.os.Handler;
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class SplashActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_splash);
+        Handler handler = new Handler();
+        handler.postDelayed(new Runnable() {
+            @Override
+            public void run() {
+                // Start the MainActivity
+                startActivity(new Intent(SplashActivity.this, MainActivity.class));
+                overridePendingTransition(R.anim.fade_in,R.anim.fade_out);
+                // Close this activity
+                finish();
+
+            }
+        }, 2500);
+    }
+}
