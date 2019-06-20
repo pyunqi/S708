117fd03f449d38c91abf25b7393e23212486edc9
pyunqi <pyunqi@gmail.com>
2019-06-21 00:12:25 +1200

    add google map

--- app/src/main/java/com/yupa/cands/stuff/AddStuff.java
+++ app/src/main/java/com/yupa/cands/stuff/AddStuff.java
@@ -1,15 +1,35 @@
 package com.yupa.cands.stuff;
 
+import android.Manifest;
+import android.app.Dialog;
+import android.content.pm.PackageManager;
 import android.net.Uri;
 import android.os.Bundle;
+import android.support.annotation.NonNull;
+import android.support.v4.app.ActivityCompat;
+import android.support.v4.content.ContextCompat;
 import android.support.v7.app.AppCompatActivity;
+import android.util.Log;
 import android.widget.ImageView;
+import android.widget.Toast;
 
+import com.google.android.gms.common.ConnectionResult;
+import com.google.android.gms.common.GoogleApiAvailability;
+import com.google.android.gms.maps.GoogleMap;
+import com.google.android.gms.maps.OnMapReadyCallback;
+import com.google.android.gms.maps.SupportMapFragment;
 import com.yupa.cands.R;
 
 import java.io.File;
 
 public class AddStuff extends AppCompatActivity {
+
+    private static final String TAG = "AddStuff_MAP";
+    private static final String COARSE_LOCTION = Manifest.permission.ACCESS_COARSE_LOCATION;
+    private static final String FINE_LOCTION = Manifest.permission.ACCESS_FINE_LOCATION;
+    private boolean mLocationPermissionGranted = false;
+    private static final int LOCATION_PERMISSION_REQUEST_CODE = 1234;
+    private GoogleMap mMap;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -19,5 +39,62 @@ public class AddStuff extends AppCompatActivity {
         File pic = (File) getIntent().getExtras().get("picFile");
         ImageView iv = findViewById(R.id.imageView);
         iv.setImageURI(Uri.fromFile(pic));
+        if (isServiceOK()) {
+            getLocationPermission();
+        }
+    }
+
+    public boolean isServiceOK() {
+        Log.d(TAG, "is ok?");
+        int available = GoogleApiAvailability.getInstance().isGooglePlayServicesAvailable(this);
+        if (available == ConnectionResult.SUCCESS) {
+            Log.d(TAG, "is ok");
+            return true;
+        } else if (GoogleApiAvailability.getInstance().isUserResolvableError(available)) {
+            Log.d(TAG, "is not good:but we can fix it.");
+            Dialog dialog = GoogleApiAvailability.getInstance().getErrorDialog(this, available, -1);
+            dialog.show();
+        } else {
+            Toast.makeText(this, "we can't make map requests", Toast.LENGTH_SHORT).show();
+        }
+        return false;
+    }
+
+    private void initMap() {
+        SupportMapFragment mapFragment = (SupportMapFragment) getSupportFragmentManager().findFragmentById(R.id.map);
+        mapFragment.getMapAsync(new OnMapReadyCallback() {
+            @Override
+            public void onMapReady(GoogleMap googleMap) {
+                mMap = googleMap;
+            }
+        });
+    }
+
+    private void getLocationPermission() {
+        String[] permissions = {Manifest.permission.ACCESS_COARSE_LOCATION, Manifest.permission.ACCESS_FINE_LOCATION};
+        if (ContextCompat.checkSelfPermission(this.getApplicationContext(), COARSE_LOCTION) == PackageManager.PERMISSION_GRANTED &&
+                ContextCompat.checkSelfPermission(this.getApplicationContext(), FINE_LOCTION) == PackageManager.PERMISSION_GRANTED) {
+            mLocationPermissionGranted = true;
+        } else {
+            ActivityCompat.requestPermissions(this, permissions, LOCATION_PERMISSION_REQUEST_CODE);
+        }
+    }
+
+    @Override
+    public void onRequestPermissionsResult(int requestCode, @NonNull String[] permissions, @NonNull int[] grantResults) {
+        mLocationPermissionGranted = false;
+        switch (requestCode) {
+            case LOCATION_PERMISSION_REQUEST_CODE: {
+                if (grantResults.length > 0) {
+                    for (int i : grantResults) {
+                        if (i != PackageManager.PERMISSION_GRANTED) {
+                            mLocationPermissionGranted = false;
+                            return;
+                        }
+                    }
+                    mLocationPermissionGranted = true;
+                }
+            }
+        }
     }
 }
